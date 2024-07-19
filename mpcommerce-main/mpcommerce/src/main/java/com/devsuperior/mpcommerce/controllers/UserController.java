7d87bad042bc8ba0c65b36bb65d6aebe0c9958e3
Mauricio Pereira <m.pereira.menezes@gmail.com>
2024-07-19 12:06:40 -0300

    adicionado PreAuthorize

--- mpcommerce-main/mpcommerce/src/main/java/com/devsuperior/mpcommerce/controllers/UserController.java
+++ mpcommerce-main/mpcommerce/src/main/java/com/devsuperior/mpcommerce/controllers/UserController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -21,6 +22,7 @@ public class UserController {
     @Autowired
     private UserService  service;
 
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_CLIENT')")
     @GetMapping(value ="/me" )
     public ResponseEntity<UserDTO> findUserMe() {
         UserDTO dto = service.getMe();
